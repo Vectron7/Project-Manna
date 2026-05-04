8873bb59485a28ebe1543866736a0329bb7c1f99
Gustavo Vieira <fonseca_vieira@educadventista.org>
2026-05-03 22:04:53 -0300

    [feat] landing page

---
+++ popup-abencoado/src/app/landingPage/landing.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import styles from './landing.module.css';
+
+export default function LandingPage() {
+  const hora = typeof window !== 'undefined' ? new Date().getHours() : 0;
+  
+  let saudacao = 'Bom dia';
+  if (hora >= 12 && hora < 18) saudacao = 'Boa tarde';
+  else if (hora >= 18 || hora < 5) saudacao = 'Boa noite';
+
+  return (
+    <main className={styles.landingContainer}>
+      <div className={styles.contentOverlay}>
+        <p className="message-calligraffitti">{saudacao}, Amado!</p>
+        <p className="message-calligraffitti">Que Deus esteja contigo!</p>
+      </div>
+    </main>
+  );
+}
---
+++ popup-abencoado/src/app/page.tsx
@@ -0,0 +1,5 @@
+import LandingPage from './landingPage/landing';
+
+export default function Home() {
+  return <LandingPage />;
+}
