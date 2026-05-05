38f651e6926d79e3916570d043434a574e103305
Gustavo Vieira <fonseca_vieira@educadventista.org>
2026-05-05 00:22:28 -0300

    [hotfix] ci/cd

--- popup-abencoado/next.config.ts
+++ popup-abencoado/next.config.ts
@@ -1,5 +1,8 @@
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: '/Project-Manna',
+  assetPrefix: '/Project-Manna',
 };
+
 export default nextConfig;
